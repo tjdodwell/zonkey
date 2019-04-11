8d55dc4ca62bd3a8c4cde9c4eeda131460d87ec9
tjdodwell <t.dodwell@exeter.ac.uk>
2019-04-11 12:07:06 +0100

    Update AM proposal

--- MCMC/Proposals/SeqAM.hh
+++ MCMC/Proposals/SeqAM.hh
@@ -26,6 +26,9 @@ Here I_d denotes the d-dimensional identity matrix.
 
 In order to start the adaptation procedure an arbitrary strictly positive definite initial covariance, C0, is chosen according to a priori knowledge (which may be quite poor).
 
+
+
+Notes Greedy Start on update Covariance on those samples which are accepted.
 */
 
 
@@ -45,28 +48,84 @@ namespace Zonkey {
 
           epsilon = 0.0001;
 
+          alpha_star = 0.25;
+
+          eps = -0.5;
+
+          C = sd * MatrixXd::Identity(STOCHASTIC_DIM,STOCHASTIC_DIM);
+
         }
 
-        void computeCov(LINK& newLink){
+        void updateC(LINK& newLink){
 
-          Eigen::Vector Xk = newLink.getTheta();
+          /*
 
-          xBar.push_back((1./(k+1)) * (k * xBar.back()  + Xk));
+          TH(:,i) = X;
+        %Update scale
+        idx = (i+(m-1)*mrun+burnin);    % Global index
+        gamma=idx^eps;
+        s =exp(log(s)+gamma*(acc_rate-optimal));
+        avg_acc = avg_acc*(i-1)/i + acc_rate/i;
+        %Update Sigma
+        delta_X=X-X_bar;    X_bar = X_bar+gamma*delta_X;
+        Sdelta=gamma*(delta_X*delta_X')-((idx-1)^eps)*V;
+        V = V+Sdelta;   L = tril(V,-1);V=diag(diag(V))+L+L';
+        [V2,errV]=chol(V,'lower');
 
+          */
 
+          int k = xBar.size()
 
+          Cold = C;
+
+          // Calculate bar{X}_k
+
+          Eigen::VectorXd Xk = newLink.getTheta();
+
+          if (k < 2 * STOCHASTIC_DIM){
+            if (k < 2){
+              xBar.push_back(Xk);
+            }
+            else{
+              xBar.push_back((1./(k+1)) * (k * xBar.back()  + Xk));
+            }
+          }
+          else{
+
+            xBar.push_back((1./(k+1)) * (k * xBar.back()  + Xk));
+
+            Eigen::MatrixXd mat1 = xBar[k-2].transpose() * xBar[k-2];
+
+            Eigen::MatrixXd mat2 = xBar[k-1].transpose() * xBar[k-1];
+
+            Eigen::MatrixXd mat3 = Xk.transpose() * Xk;
+
+            Eigen::MatrixXd mat4 = epsilon * MatrixXd::Identity(STOCHASTIC_DIM,STOCHASTIC_DIM);
+
+            C = ((k-1) / k) * Cold + (sd / k) * ( k * mat1 - (k + 1) * mat2 + mat3 + mat4);
+
+          }
 
         }
 
         LINK apply(const LINK& currentState){
+
           Eigen::VectorXd xi = currentState.getTheta();
+
+          updateC(currentState);
+
+          // Make Proposal
           Eigen::VectorXd xip(xi.size());
           std::random_device rd;
           std::normal_distribution<double> dis(0.0,1.0);
           std::mt19937 gen(rd());
+          Eigen::VectorXd gauss(xi.size());
           for (int i = 0; i < xi.size(); i++){
-            xip(i) = std::sqrt(1 - param(0) * param(0)) * xi(i) + param(0) * param(1) * dis(gen);
+            gauss(i) =  dis(gen);
           }
+          // Compute Chol of  C
+          Eigen::MatrixXd L =  C.llt().matrixL();
+          xip = xi  + std::sqrt(sd) * (L * gauss);
           LINK prop;
           prop.setTheta(xip);
           return prop; // Return Proposal from SeqRandomWalk
@@ -83,6 +142,16 @@ namespace Zonkey {
           double logalpha = std::log(dis(gen));
 
           double logtestProbability = v.getlogPhi() - u.getlogPhi();
+
+          // In this step we update scaling.
+
+          // Update Scaling for proposal
+
+          double gamma = (double) std::pow(xBar.size(),eps);
+
+          double log_sd = std::log(sd) + gamma * (std::exp(logalpha) - alpha_star);
+
+          sd = std::exp(sd);
 
           if (logalpha < logtestProbability){
             accept = true;
@@ -109,8 +178,12 @@ namespace Zonkey {
 
         Eigen::VectorXd param; // Step size for random step
 
+        Eigen::MatrixXd C, Cnew;
 
-        double sd, epsilon;
+
+        std::vector<Eigen::VectorXd> Xbar;
+
+        double sd, epsilon, eps, alpha_star;
 
 
     };
