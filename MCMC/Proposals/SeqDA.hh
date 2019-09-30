9783d3ba76c55af2c37cbd9321dc1b136f1a0126
tjdodwell <t.dodwell@exeter.ac.uk>
2019-09-30 22:39:03 +0100

    Few bugs corrected in SeqDA

--- MCMC/Proposals/SeqDA.hh
+++ MCMC/Proposals/SeqDA.hh
@@ -11,45 +11,47 @@ namespace Zonkey {
 
       public:
 
-        SeqDA(PROPOSAL& myProposal_, ForwardModel& F_, int level_ = 1, int subChain_length_ = 10) :
+        SeqDA(PROPOSAL& myProposal_, ForwardModel& F_, int level_ = 1, int subChain_length_ = 10, bool adaptive_ = false) :
           subChain_length(subChain_length_),
           myProposal(myProposal_),
           F(F_),
-          level(level_){
+          level(level_),
+          adaptive(adaptive_){
 
             count = 0;
-            sig_pcn = myProposal.getScaling();
+
 
 
         }
 
         LINK apply(LINK& currentState){
 
-          Eigen::VectorXd xi = currentState.getTheta();
+          Eigen::VectorXd xi = currentState.getTheta(); // Obtain current point
 
           CHAIN markovChain; // Setup a sub chain
 
-          MetropolisHastings<LINK,CHAIN,PROPOSAL,ForwardModel> myMCMC(F,myProposal,markovChain);
+          MetropolisHastings<LINK,CHAIN,PROPOSAL,ForwardModel> myMCMC(F,myProposal,markovChain); // Setup up subChain
 
-          myMCMC.setStart(xi,level-1);
+          myMCMC.setStart(xi,level-1); // Initate start point
 
           myMCMC.run(subChain_length,level-1,"",false);
 
-          auto theChain = myMCMC.getChain();
+          auto theChain = myMCMC.getChain(); // extract the chain
 
-          logCoarse = theChain[0].getlogPhi();
-          myMCMC.run(subChain_length-1,level-1,"",false);
+          logCoarse = theChain[0].getlogPhi(); // Obtain Log logCoarse
 
-          auto mc = myMCMC.getChain();
+          auto prop = theChain.back();
 
-          auto prop = mc.back();
           logCoarseProp = prop.getlogPhi();
+
           return prop; // Return Proposal from SeqDA
+
         }
 
         bool acceptReject(LINK& u,  LINK& v, bool verb = false){
 
           bool accept = false;
+
 
           std::random_device rd;
           std::mt19937 gen(rd());
@@ -58,6 +60,7 @@ namespace Zonkey {
           double logalpha = std::log(dis(gen));
 
           double logtestProbability = v.getlogPhi(false) + logCoarse - u.getlogPhi(false) - logCoarseProp;
+
 
           if (logalpha < logtestProbability){
             accept = true;
@@ -68,8 +71,8 @@ namespace Zonkey {
             std::cout << "logalpha = " << logalpha << std::endl;
             std::cout << "logtestProbability = " << logtestProbability << std::endl;
             std::cout << "Do we accept ? = " << accept << std::endl;
-            std::cout << "proposal ll = " << v.getlogPhi() << std::endl;
-            std::cout << "existing ll = " << u.getlogPhi() << std::endl;
+            std::cout << "proposal ll = " << v.getlogPhi(false) << std::endl;
+            std::cout << "existing ll = " << u.getlogPhi(false) << std::endl;
 
           }
 
@@ -94,7 +97,7 @@ namespace Zonkey {
 
         int count, level;
 
-        double sig_pcn;
+        int adaptive;
 
 
     };
